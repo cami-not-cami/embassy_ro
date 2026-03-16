9379e4afe3e899c9fc2dcdaaa5b12f6b3db4ab28
Camelia Ciocan <camelia-carmen.ciocan@oebb.at>
2026-03-16 07:20:53 +0100

    commit for merge

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const mysql = require('mysql2');
+const url = require("url");
+
+const app = express();
+const port = 8080;
+let i18n;
+async function startServer() {
+    const { I18n } = await import("i18n-js");
+
+    i18n = new I18n({
+        ro: { home: "Acasă", search: "Căutare", romania:"România", contact:"Contactaţi-ne" },
+        de: { home: "Startseite", search: "Suche", romania: "Romenien", contact:"Kontakt" }
+    });
+
+    app.use(express.urlencoded({ extended: true }));
+
+    // Serve only JS/CSS as static, NOT the html folder
+    app.use('/js', express.static(path.join(__dirname, '../client/js')));
+
+    app.get("/", (req, res) => {
+        const locale = req.query.lang || "de";
+        i18n.locale = locale;
+
+        //reads html file and replaces the keywords with translated strings
+        let html = fs.readFileSync(path.join(__dirname, '../client/html/index.html'), "utf-8");
+        html = html.replace(/{{(\w+)}}/g, (_, key) => i18n.t(key));
+
+        res.send(html);
+
+        let pathQuery = url.parse(req.url, true);
+        console.log(path);
+
+        switch (path.pathname) {
+            case "/dbq":{
+
+            }break;
+            case "/db":
+            {
+
+            }break;
+            default: {
+                res.sendFile(__dirname + "/client"+ path.pathname, (err) => {
+                    if (err) {
+                        console.log(err);
+                        res.send(err.message);
+                    }
+                });
+            }
+                break;
+
+        }
+    });
+
+    app.listen(port, () => {
+        console.log(`App listening at http://localhost:${port}`);
+    });
+}
+
+// MySQL
+const con = mysql.createConnection({
+    host: "bucket-deny.with.playit.plus",
+    user: "cami_app",
+    password: "pass123",
+    port: 25770
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+startServer();
