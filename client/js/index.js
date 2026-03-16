ef146c85a52d5fa6ac9bbb884a79297e088f57ee
Camelia Ciocan <camelia-carmen.ciocan@oebb.at>
2026-03-16 14:19:25 +0100

    can create a user now

--- client/js/index.js
+++ client/js/index.js
@@ -1,27 +1,34 @@
 
 document.addEventListener("DOMContentLoaded", async function () {
-document.getElementById("search").addEventListener("click", createUser)
-document.getElementById("btnLogin").addEventListener("click", createUser)
 
-    document.getElementById("btnLogOut").addEventListener("click", createUser)
+
     document.getElementById("btnSignUp").addEventListener("click", createUser)
-// fetch('/dbq?id=1')
-//     .then(res => res.json())
-//     .then(data => console.log(data));
+
 })
  async function createUser() {
-    const res = await fetch('/users', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            firstname: "Cami",
-            lastname: "Doe",
-            email: "john@gmail.com",
-            password: "password",
+     let  firstname= document.getElementById("inputFirstName").value
+     let lastname= document.getElementById("inputLastName").value
+     let email=  document.getElementById("inputSignupEmail").value
+     let password=  document.getElementById("inputSignupPassword").value
+     let confirmPassword=  document.getElementById("inputSignupConfirmPassword").value
+     if(password !== confirmPassword) {
+         alert("Passwords do not match")
+     }
+     else if(password === confirmPassword) {
+         const res = await fetch('/users', {
+             method: 'POST',
+             headers: { 'Content-Type': 'application/json' },
+             body: JSON.stringify({
+                 firstname: document.getElementById("inputFirstName").value,
+                 lastname: document.getElementById("inputLastName").value,
+                 email:  document.getElementById("inputSignupEmail").value,
+                 password:  document.getElementById("inputSignupPassword").value,
 
-        })
-    });
+             })
+         });
+     }
 
-    const data = await  res.json()
-    console.log(data); // { id: 2 }
+
+
+
 }
